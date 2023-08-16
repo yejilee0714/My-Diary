05c78ab195dd658e200814a903a8cf89f045ecf3
yejilee0714 <ellie.yejilee@gmail.com>
2023-08-17 00:43:30 +0900

    #7 design: 버튼, 상단 헤더 디자인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import MainHeader from "./components/header/MainHeader";
+import UploadHeader from "./components/header/UploadHeader";
+import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainHeader />
+      <UploadHeader />
+      <GlobalStyle />
+    </>
   );
 }
 
