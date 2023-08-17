c0ddc5ae58d9c086ff4afac67ceb3eb41e205ad7
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-08-18 00:32:35 +0900

    Revert "#design: 공통 컴포넌트 개발"

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import MainHeader from "./components/header/MainHeader";
-import UploadHeader from "./components/header/UploadHeader";
-import GlobalStyle from "./style/GlobalStyle";
-import Navigation from "./components/common/Navbar";
-import Modal from "./components/modal/Modal";
-import AlertModal from "./components/modal/PopupModal";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-    <>
-      <Navigation />
-      <Routes>
-          <Route path="/calendar" />
-          <Route path="/diary" />
-          <Route path="/todo-list" />
-        </Routes>
-      <Modal />
-      <AlertModal
-          submitText="로그아웃"
-          // onSubmit={() => {
-          //   logoutHandler();
-          //   setModalOpen(false);
-          // }}
-          // onCancel={() => setModalOpen(false)}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          로그아웃하시겠어요?
-        </AlertModal>
-      <GlobalStyle />
-    </>
-
-    </Router>
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
