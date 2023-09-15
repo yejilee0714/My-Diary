0d36765ac815a03c67666b18e1e98b79263cf639
yejilee0714 <ellie.yejilee@gmail.com>
2023-09-15 21:45:05 +0900

    design: #19 header에 more 아이콘 클릭 시 하단 모달창 나오도록 구현

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import { ReactComponent as IconProfile } from '../../assets/img/profile-main.svg'
-import { ReactComponent as IconMore } from '../../assets/icon/more.svg'
+import moreicon from '../../assets/icon/more.svg'
+import Modal from '../Modal/Modal'
 
 export default function MainHeader(){
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return(
-    <BasicHeaderStyle>
-      <IconProfile style={{ cursor:'pointer', height: '100%'}}/>
-      <IconMore style={{ cursor:'pointer', height: '100%'}}/>
-    </BasicHeaderStyle>
+    <>
+      <BasicHeaderStyle>
+        <IconProfile style={{ cursor:'pointer', height: '100%'}}/>
+        {/* <IconMore style={{ cursor:'pointer', height: '100%'}}/> */}
+        <IconMore >
+          <button onClick={openModal}></button>
+        </IconMore>
+        {/* 모달 */}
+      </BasicHeaderStyle>
+      {isModalOpen && <Modal closeModal={closeModal} />}
+    </>
   )
 }
 
@@ -19,7 +38,16 @@ const BasicHeaderStyle = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: 5px;
+  padding: 5px 0;
   background-color: var(--main-color);
   z-index: 1;
+`
+
+const IconMore = styled.div`
+  button {
+    width: 100%;
+    height: 100%;
+    padding: 35px 40px;
+    background: url(${moreicon}) center/cover no-repeat;
+  }
 `
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState } from 'react';
 import styled from "styled-components";
 import '../../style/font.css'
 
 import { useNavigate } from 'react-router-dom';
 
-export default function Modal(){
+export default function Modal({closeModal }){
   const navigate = useNavigate();
 
-
   return (
-      <ModalStyle>
-        <ul>
+    <ModalStyle>
+      <ul >
         <div className='bar'></div>
         <li>
             <button type='button' onClick={(e) => {
-                navigate('/profile');
-              }}
+              navigate('/profile');
+            }}
             > 프로필 수정
             </button>
           </li>
@@ -24,6 +23,10 @@ export default function Modal(){
                 navigate('/');
               }}
             > 로그아웃
+            </button>
+          </li>
+          <li>
+            <button type='button' onClick={closeModal}> 취소
             </button>
           </li>
         </ul>
@@ -36,6 +39,9 @@ const ModalStyle = styled.div`
   position: fixed;
   width: 100%;
   height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 1;
 
   .bar{
     width: 50px;
@@ -53,9 +59,12 @@ const ModalStyle = styled.div`
     flex-direction: column;
     bottom: 0;
     padding-bottom: 20px;
+    border-radius: 20px 20px 0 0;
   }
   
   button{
+    width: 100%;
+    text-align: left;
     padding: 14px 26px;
   }
 `
